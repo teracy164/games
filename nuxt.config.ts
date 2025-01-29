3060dfaaa8c9d40dc5296488423e39003fd04ff9
teracy164 <literacy1945@gmail.com>
2025-01-29 13:29:26 +0900

    開発時の設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: './src',
+  devServer: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT || 3000),
+  },
   css: ['@/assets/css/styles.scss'],
   modules: ['@element-plus/nuxt', '@nuxtjs/sitemap', '@nuxtjs/tailwindcss', '@nuxt/image'],
   elementPlus: {
