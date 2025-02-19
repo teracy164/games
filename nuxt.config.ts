ea79f181b34e9ac4bf89cede5d3bc8ff1e1e093c
teracy164 <literacy1945@gmail.com>
2025-02-19 19:09:29 +0900

    フレームワーク最新化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,34 +5,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: './src',
-  devServer: {
-    host: process.env.HOST || '0.0.0.0',
-    port: Number(process.env.PORT || 3000),
-  },
+  // devServer: {
+  //   host: process.env.HOST || '0.0.0.0',
+  //   port: Number(process.env.PORT || 3000),
+  // },
   css: ['@/assets/css/styles.scss'],
   modules: ['@element-plus/nuxt', '@nuxtjs/sitemap', '@nuxtjs/tailwindcss', '@nuxt/image'],
   elementPlus: {
     /** Options */
   },
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'ja',
-      },
-    },
-  },
-  site: {
-    url: 'https://games.teracy.link/',
-    name: SERVICE_NAME,
-    trailingSlash: true,
-  },
-  experimental: {
-    defaults: {
-      nuxtLink: {
-        trailingSlash: 'append',
-      },
-    },
-  },
+  app: { head: { htmlAttrs: { lang: 'ja' } } },
+  site: { url: 'https://games.teracy.link/', name: SERVICE_NAME, trailingSlash: true },
+  experimental: { defaults: { nuxtLink: { trailingSlash: 'append' } } },
   // nitro: {
   //   prerender: {
   //     failOnError: false,
