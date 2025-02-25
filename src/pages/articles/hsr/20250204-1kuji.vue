e97c32526d1ec9a2e20c7a98abd0dd14715769a3
teracy164 <literacy1945@gmail.com>
2025-02-25 21:00:18 +0900

    ニュースのテンプレート化

--- src/pages/articles/hsr/20250204-1kuji.vue
+++ src/pages/articles/hsr/20250204-1kuji.vue
@@ -1,90 +1,75 @@
 <template>
-  <div class="flex justify-center">
-    <Card class="grow" style="width: 100%; max-width: 800px">
-      <div class="relative">
-        <h1 class="bg-red-500 p-2">
-          <label>{{ news.title }}</label>
-        </h1>
-        <div class="absolute text-sm text-gray-500" style="bottom: 100%; right: 0; font-size: 0.7em; line-height: 1em">
-          {{ news.createdAt }}
+  <ArticleTemplate :news="news">
+    <div class="mb-5">
+      崩壊スターレイルの一番くじの再販売が決定！！！
+      <br />
+      1回780円（税込）で、一番くじONLINEでの販売となります。
+      <br />
+      <br />
+      A賞からC賞は光円錐のタペストリー、D賞は開拓者おなじみのゴミ箱のマグカップです！！
+      <br />
+      E賞以降もマルチクロスやアクリルチャーム、ラバーコースターと魅力的なラインナップとなっております。
+
+      <div class="my-5">
+        <div class="w-full flex flex-col justify-center items-center">
+          <Tweet tweet-id="1886338955446243466" style="width: 500px; max-width: 90vw" />
         </div>
       </div>
 
-      <div class="flex justify-end my-2">
-        <ShareBtnTwitter v-if="href" :title="news.title" :hash-tags="news.hashTags" :page-url="href" />
-      </div>
+      <p>さらにダブルチャンスキャンペーンでラストワン章のイラストボードが当たるチャンスもあります！！</p>
 
-      <div class="mb-5">
-        崩壊スターレイルの一番くじの再販売が決定！！！
-        <br />
-        1回780円（税込）で、一番くじONLINEでの販売となります。
-        <br />
-        <br />
-        A賞からC賞は光円錐のタペストリー、D賞は開拓者おなじみのゴミ箱のマグカップです！！
-        <br />
-        E賞以降もマルチクロスやアクリルチャーム、ラバーコースターと魅力的なラインナップとなっております。
+      <h3 class="mt-5"><label class="bg-red-400 px-2">詳細情報</label></h3>
+      <table class="item-info">
+        <tbody>
+          <tr>
+            <th>発売日</th>
+            <td>2025年04月23日 17時より</td>
+          </tr>
+          <tr>
+            <th>価格</th>
+            <td>
+              <p>1回780円(税込)</p>
+            </td>
+          </tr>
+          <tr>
+            <th>各等賞</th>
+            <td>
+              <ul>
+                <li>A賞 夜明け前 タペストリー</li>
+                <li>B賞 この身は剣なり タペストリー</li>
+                <li>C賞 陽光より輝くもの タペストリー</li>
+                <li>D賞 マグカップ</li>
+                <li>E賞 マルチクロス</li>
+                <li>F賞 アクリルチャーム</li>
+                <li>G賞 ラバーコースター</li>
+                <li>ラストワン賞 イラストボードセット</li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th>販売店</th>
+            <td>一番くじONLINE</td>
+          </tr>
+          <tr>
+            <th>販売ページ</th>
+            <td>
+              <el-link type="primary">
+                <a href="https://1kuji.com/products/houkaistarrail-2">
+                  一番くじ 崩壊：スターレイル｜一番くじ倶楽部｜BANDAI SPIRITS公式 一番くじ情報サイト
+                </a>
+              </el-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <AdsCarouselAmiami class="my-5" />
 
-        <div class="my-5">
-          <div class="w-full flex flex-col justify-center items-center">
-            <Tweet tweet-id="1886338955446243466" style="width: 500px; max-width: 90vw" />
-          </div>
-        </div>
-
-        <p>さらにダブルチャンスキャンペーンでラストワン章のイラストボードが当たるチャンスもあります！！</p>
-
-        <h3 class="mt-5"><label class="bg-red-400 px-2">詳細情報</label></h3>
-        <table class="item-info">
-          <tbody>
-            <tr>
-              <th>発売日</th>
-              <td>2025年04月23日 17時より</td>
-            </tr>
-            <tr>
-              <th>価格</th>
-              <td>
-                <p>1回780円(税込)</p>
-              </td>
-            </tr>
-            <tr>
-              <th>各等賞</th>
-              <td>
-                <ul>
-                  <li>A賞 夜明け前 タペストリー</li>
-                  <li>B賞 この身は剣なり タペストリー</li>
-                  <li>C賞 陽光より輝くもの タペストリー</li>
-                  <li>D賞 マグカップ</li>
-                  <li>E賞 マルチクロス</li>
-                  <li>F賞 アクリルチャーム</li>
-                  <li>G賞 ラバーコースター</li>
-                  <li>ラストワン賞 イラストボードセット</li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <th>販売店</th>
-              <td>一番くじONLINE</td>
-            </tr>
-            <tr>
-              <th>販売ページ</th>
-              <td>
-                <el-link type="primary">
-                  <a href="https://1kuji.com/products/houkaistarrail-2">
-                    一番くじ 崩壊：スターレイル｜一番くじ倶楽部｜BANDAI SPIRITS公式 一番くじ情報サイト
-                  </a>
-                </el-link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <AdsCarouselAmiami class="my-5" />
-
-      <div class="flex flex-col justify-center items-center">
-        <img src="https://sf.1kuji.com/page/houkaistarrail/0801/202408_star_lineup_0725_full.jpg" />
-        <p>© Copyright (C) miHoYo. All Rights Reserved</p>
-      </div>
-    </Card>
-  </div>
+    <div class="flex flex-col justify-center items-center">
+      <img src="https://sf.1kuji.com/page/houkaistarrail/0801/202408_star_lineup_0725_full.jpg" />
+      <p>© Copyright (C) miHoYo. All Rights Reserved</p>
+    </div>
+  </ArticleTemplate>
 </template>
 <script lang="ts" setup>
 import { NEWS } from '~/shared/articles';
@@ -99,29 +84,4 @@ const news = NEWS.find((item) => item.id === pageId);
 if (!news) {
   navigateTo('/');
 }
-
-const description = [news.description].join('\n');
-useSeoMeta({ title: news.title, ogTitle: news.title, description, ogDescription: description });
-
-const href = ref('');
-onMounted(() => {
-  href.value = location?.href;
-});
 </script>
-<style lang="scss" scoped>
-table.item-info {
-  th,
-  td {
-    border: 1px solid lightgray;
-    padding: 0.25em;
-    word-break: break-all;
-  }
-  th {
-    min-width: 5em;
-    text-align: left;
-  }
-  li + li {
-    margin-top: 0.5em;
-  }
-}
-</style>
--- src/pages/articles/hsr/20250205-update.vue
+++ src/pages/articles/hsr/20250205-update.vue
@@ -1,67 +1,52 @@
 <template>
-  <div class="flex justify-center">
-    <Card class="grow" style="width: 100%; max-width: 800px">
-      <div class="relative">
-        <h1 class="bg-red-500 p-2">
-          <label>{{ news.title }}</label>
-        </h1>
-        <div class="absolute text-sm text-gray-500" style="bottom: 100%; right: 0; font-size: 0.7em; line-height: 1em">
-          {{ news.createdAt }}
+  <ArticleTemplate :news="news">
+    <div class="mb-5">
+      崩壊スターレイル公式Xの開拓者ラジオにて今後のアップデートについて発表されました。
+      <br />
+      主な内容はオンパロスのストーリーに関するものですが、その中に気になる１文がありました。
+
+      <div class="my-5">
+        <div class="w-full flex flex-col justify-center items-center">
+          <Tweet tweet-id="1887003217659089117" style="width: 500px; max-width: 90vw" />
         </div>
       </div>
+      <p>
+        なんと、
+        <span class="text-red-300">
+          「過去に登場したキャラクターを先頭に出しづらい」という問題に対し、キャラクターの強化や調整を行っていく
+        </span>
+        と明言されています！
+      </p>
 
-      <div class="flex justify-end my-2">
-        <ShareBtnTwitter v-if="href" :title="news.title" :hash-tags="news.hashTags" :page-url="href" />
-      </div>
+      <AdsCarouselAmiami class="my-5" />
 
-      <div class="mb-5">
-        崩壊スターレイル公式Xの開拓者ラジオにて今後のアップデートについて発表されました。
+      <div>
         <br />
-        主な内容はオンパロスのストーリーに関するものですが、その中に気になる１文がありました。
-
-        <div class="my-5">
-          <div class="w-full flex flex-col justify-center items-center">
-            <Tweet tweet-id="1887003217659089117" style="width: 500px; max-width: 90vw" />
-          </div>
-        </div>
         <p>
-          なんと、
-          <span class="text-red-300">
-            「過去に登場したキャラクターを先頭に出しづらい」という問題に対し、キャラクターの強化や調整を行っていく
-          </span>
-          と明言されています！
+          最近ではホタルを始めとした靭性無視のキャラが実装されて銀狼の弱点を埋め込みが不要になったり、
+          ゼーレや彦卿といった初期のアタッカーが現環境では火力不足だったりと明らかに出番が少なくなっているキャラが増えていました。
+          <br />
+          <br />
+          私自身カフカが推しなんですが、虚無・雷のアタッカーだと黄泉が強すぎて出番がなくて寂しさを感じていました。
+          <br />
+          <br />
+          強化の内容については言及されていないので、続報に期待しましょう！！
         </p>
-
-        <AdsCarouselAmiami class="my-5" />
-
-        <div>
-          <br />
-          <p>
-            最近ではホタルを始めとした靭性無視のキャラが実装されて銀狼の弱点を埋め込みが不要になったり、
-            ゼーレや彦卿といった初期のアタッカーが現環境では火力不足だったりと明らかに出番が少なくなっているキャラが増えていました。
-            <br />
-            <br />
-            私自身カフカが推しなんですが、虚無・雷のアタッカーだと黄泉が強すぎて出番がなくて寂しさを感じていました。
-            <br />
-            <br />
-            強化の内容については言及されていないので、続報に期待しましょう！！
-          </p>
-          <br />
-          <p>詳細はHoYoLABをご覧ください</p>
-          <el-link type="primary">
-            <a href="https://www.hoyolab.com/article/36867900?utm_source=sns&utm_medium=twitter&utm_id=6">
-              開発者ラジオ | Ver.3.0 オンパロス特別編 崩壊：スターレイル | HoYoLAB
-            </a>
-          </el-link>
-        </div>
-        <!-- <p>
+        <br />
+        <p>詳細はHoYoLABをご覧ください</p>
+        <el-link type="primary">
+          <a href="https://www.hoyolab.com/article/36867900?utm_source=sns&utm_medium=twitter&utm_id=6">
+            開発者ラジオ | Ver.3.0 オンパロス特別編 崩壊：スターレイル | HoYoLAB
+          </a>
+        </el-link>
+      </div>
+      <!-- <p>
           <img
             src="https://upload-os-bbs.hoyolab.com/upload/2025/02/05/228ac9ac21e59c3237d17ea6fbbbeecc_5179631966579274644.jpg?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
           />
         </p> -->
-      </div>
-    </Card>
-  </div>
+    </div>
+  </ArticleTemplate>
 </template>
 <script lang="ts" setup>
 import { NEWS } from '~/shared/articles';
@@ -76,20 +61,4 @@ const news = NEWS.find((item) => item.id === pageId);
 if (!news) {
   navigateTo('/');
 }
-
-const description = [news.description].join('\n');
-useSeoMeta({ title: news.title, ogTitle: news.title, description, ogDescription: description });
-
-const href = ref('');
-onMounted(() => {
-  href.value = location?.href;
-});
 </script>
-<style lang="scss" scoped>
-img {
-  width: 100%;
-  height: 800px;
-  object-fit: cover;
-  object-position: 50% 96%;
-}
-</style>
